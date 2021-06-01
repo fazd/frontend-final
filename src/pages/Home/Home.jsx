d5da43b0d4676fb6e3fab31c5940339c9921d50b
seviloria <seviloria@uninorte.edu.co>
2021-05-31 21:20:21 -0500

    añadiendo home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from '../../components/navbar/Navbar';
 import Footer from '../../components/footer/Footer';
+import './style.css';
 
 const Home = () => {
   return (
@@ -8,6 +9,30 @@ const Home = () => {
       <Navbar />
       <div className="page">
         <span>Home works</span>
+        <div class="lugares_home">
+          <div class="texto_lugar_home">
+            <img src="ventana.jpg" alt="foto_ventana" class="imagen_home" />
+            <p class="texto_imagenes">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore officia accusamus iusto, suscipit vel quo recusandae quod dicta quis beatae debitis maxime aspernatur veniam, reprehenderit odit natus asperiores harum amet?
+            </p>
+          </div>
+        </div>
+        <div class="lugares_home2">
+          <div class="texto_lugar_home2">
+            <img src="../../assets/iglesia.jpg" alt="foto_ventana" class="imagen_home" />
+            <p class="texto_imagenes">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore officia accusamus iusto, suscipit vel quo recusandae quod dicta quis beatae debitis maxime aspernatur veniam, reprehenderit odit natus asperiores harum amet?
+            </p>
+          </div>
+        </div>
+        <div class="lugares_home3">
+          <div class="texto_lugar_home">
+            <img src="caiman.jpg" alt="foto_ventana" class="imagen_home" />
+            <p class="texto_imagenes">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore officia accusamus iusto, suscipit vel quo recusandae quod dicta quis beatae debitis maxime aspernatur veniam, reprehenderit odit natus asperiores harum amet?
+            </p>
+          </div>
+        </div>
       </div>
       <Footer />
     </div>
