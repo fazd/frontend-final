b54e04c5c6ee52385ffcf9d7f2f36e6ee5660bea
fazd <fabioz@uninorte.edu.co>
2021-06-05 00:09:15 -0500

    add card to recommendations

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,23 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import { Link } from 'react-router-dom';
+const Card = (props) => {
+  return (
+    <Link className={'link-card ' + props.className} to={`/${props.type}/${props.id}`}>
+      <div class="card card-vertical shadow p-3bg-body">
+        <img src={props.img} class="card-img-top card-img mt-3" alt="img" />
+        <div class="card-body">
+          <h5 class="card-title">{props.name}</h5>
+          <p class="card-text">{props.content}</p>
+          <div className="d-flex justify-content-end align-items-center">
+            <FontAwesomeIcon icon="thumbs-up" color="gray" className="me-3" /> {props.thumbsUp}
+            <FontAwesomeIcon icon="thumbs-down" color="gray" className="ms-3 me-3" /> {props.thumbsDown}
+          </div>
+        </div>
+
+      </div>
+
+    </Link>
+  );
+}
+export default Card;
--- src/pages/Place/Place.jsx
+++ src/pages/Place/Place.jsx
@@ -4,30 +4,31 @@ import Navbar from '../../components/navbar/Navbar';
 import img from '../../assets/ventana-al-mundo.jpg'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Comment from '../../components/comment/Comment';
-import {places} from '../../services/places.json';
+import { places } from '../../services/places.json';
 import { useParams } from 'react-router-dom';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import markerIconPng from "leaflet/dist/images/marker-icon.png"
-import {Icon} from 'leaflet'
+import { Icon } from 'leaflet'
 import CommentBox from '../../components/comment-box/Coment-box';
 import ReactStars from 'react-stars'
+import Card from '../../components/card/Card';
 
 
 const Place = () => {
 
-  const {id} = useParams();
+  const { id } = useParams();
   const [place, setPlace] = useState({});
   const [comments, setComments] = useState([]);
   const [rating, setRating] = useState([]);
 
-  useEffect(()=> {
+  useEffect(() => {
     const response = places.find((place) => place.id === parseInt(id));
     console.log(response);
     console.log(id);
     setPlace(response);
     setComments(response.comments);
 
-  },[]);
+  }, [place, id]);
 
   const handleNewComment = (e) => {
     const aux = comments;
@@ -92,11 +93,11 @@ const Place = () => {
                   attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                   url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker 
+                <Marker
                   position={[place.positionX, place.positionY]}
-                  icon={new Icon({iconUrl: markerIconPng, iconSize: [25, 41], iconAnchor: [12, 41]})}
+                  icon={new Icon({ iconUrl: markerIconPng, iconSize: [25, 41], iconAnchor: [12, 41] })}
                 />
-                  
+
               </MapContainer>
 
             </div> : null}
@@ -115,14 +116,14 @@ const Place = () => {
                   place.sliderUrls?.map((link, index) => {
                     const active = index === 0 ? 'active' : '';
                     return (
-                      
+
                       <div className={`carousel-item ${active}`}>
                         <img src={link} className="d-block w-100" alt="..." />
                       </div>
                     )
-                  }) 
+                  })
                 }
-                
+
               </div>
               <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
                 <span className="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -136,22 +137,73 @@ const Place = () => {
             <h3 className="mt-3">Comentarios </h3>
             <div className="comment-group mb-4">
               <CommentBox newComment={handleNewComment} />
-              { 
+              {
                 comments?.map((comment) => {
                   return (
-                    <Comment 
+                    <Comment
                       name={comment.user}
                       created={comment.created}
                       content={comment.content}
                       heartQuantity={comment.heart}
                       thumbsUpQuantity={comment.thumbsUp}
                       thumbsDownQuantity={comment.thumbsDown}
-                    /> 
+                    />
                   );
                 })
               }
 
             </div>
+
+          </div>
+        </div>
+        <div className="row mt-5  d-flex justify-content-around mb-5">
+          <h2 className="text-center mb-5">Tambien te puede interesar</h2>
+          <div className="d-flex flex-row justify-content-evenly">
+            {
+              place.recomendations?.map((rec) =>
+                <Card
+                  img={rec.img}
+                  name={rec.name}
+                  content={rec.content}
+                  id={rec.id}
+                  type={rec.type}
+                  thumbsUp={rec.thumbsUp}
+                  thumbsDown={rec.thumbsDown}
+                  className="mx-3"
+                />
+              )
+            }
+            {/* <Card
+              img={'https://upload.wikimedia.org/wikipedia/commons/7/74/Iglesia_de_San_Nicolas_de_Tolentino_%28Barranquilla%29.jpg'}
+              name={'Iglesia de San nicolas'}
+              content={'Se que te puede interesar'}
+              id={'30'}
+              type='event'
+              thumbsUp={30}
+              thumbsDown={30}
+              className="mx-3"
+            />
+            <Card
+              img={'https://upload.wikimedia.org/wikipedia/commons/7/74/Iglesia_de_San_Nicolas_de_Tolentino_%28Barranquilla%29.jpg'}
+              name={'Iglesia de San nicolas'}
+              content={'Se que te puede interesar'}
+              id={'30'}
+              type='event'
+              thumbsUp={30}
+              thumbsDown={30}
+              className="mx-3"
+            />
+            <Card
+              img={'https://upload.wikimedia.org/wikipedia/commons/7/74/Iglesia_de_San_Nicolas_de_Tolentino_%28Barranquilla%29.jpg'}
+              name={'Iglesia de San nicolas'}
+              content={'Se que te puede interesar'}
+              id={'30'}
+              type='event'
+              thumbsUp={30}
+              thumbsDown={30}
+              className="mx-3"
+            /> */}
+
 
           </div>
         </div>
